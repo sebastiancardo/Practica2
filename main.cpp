39264268a9e941c54a43898ec615d0c960435005
sebastiancardo <53200453+sebastiancardo@users.noreply.github.com>
2019-09-08 23:18:01 -0500

    Add files via upload
    punto #2

--- main.cpp
+++ main.cpp
@@ -1,72 +1,44 @@
 #include <iostream>
-#include "cmath"
-#include "complejo.h"
+#include "vertice.h"
 using namespace std;
-double Modulo(Complejo c1);
+
+//Desarrolle una pequeña aplicación en la que se puedan crear distintas figuras geométricas
+//y averiguar su perímetro y su área.
+
+//"Circulo", "Triangulo", "Rectangulo", "Pentagono" y "Hexagono"
 
 int main()
 {
-    double a=0;//real complejo1
-    double b=0;//imaginario complejo 1
-    double c=0;//real complejo 2
-    double d=0;//imaginario complejo 2
-    cout<<" Porfavor ingrese la parte real del primer numero complejo"<<endl;
-    cin>>a;
-    cout<<" Porfavor ingrese la parte imaginaria del primer numero complejo"<<endl;
-    cin>>b;
-    cout<<" Porfavor ingrese la parte real del segundo numero complejo"<<endl;
-    cin>>c;
-    cout<<" Porfavor ingrese la parte imaginaria del segundo  numero complejo"<<endl;
-    cin>>d;
-    cout<<"Por favor ingrese la operacion que desea hacer:"<<endl;
-    cout<<"ingrese 1 para sumar los complejos "<<endl;
-    cout<<"ingrese 2 para restar los complejos "<<endl;
-    cout<<"ingrese 3 para multiplicar los complejos "<<endl;
-    cout<<"ingrese 4 para dividir los complejos "<<endl;
-    cout<<"ingrese 5 para realizar el conjugado del primer numero complejo"<<endl;
-    cout<<"ingrese 6 para realizar el conjugado del segundo numero complejo"<<endl;
-    cout<<"ingrese 7 para realizar el modulo a el primer numero complejo"<<endl;
-    cout<<"ingrese 8 para realizar el modulo a el segundo numero complejo"<<endl;
-    Complejo c1(a,b);
-    Complejo c2(c,d);
-   int opcion=0;
-   cin>>opcion;
-    switch (opcion)
-    {case 1:{cout<<"el resultado de la suma es:";c1+c2;}
+    cout << endl<<"             *****Programa para crear figuras geometricas*****"<<endl<<endl;
+
+    cout<<"¿Cual de las siguientes figuras desea crear?"<<endl<<endl;
+
+    cout<<"Ingrese:"<<endl<<endl;
+    cout<<"1. Para Círculo"<<endl;
+    cout<<"2. Para Triángulo"<<endl;
+    cout<<"3. Para Rectángulo"<<endl;
+    cout<<"4. Para Pentágono"<<endl;
+    cout<<"5. Para Hexágono"<<endl<<endl;
+
+    int opcion_menu=0;
+
+    cin>>opcion_menu;
+
+    switch(opcion_menu) {
+
+    case 1: {cout<<"1"<<endl;}
         break;
-    case 2:{cout<<"el resultado de la resta es:";c1-c2;}
+    case 2: {cout<<"2"<<endl;}
         break;
-    case 3:{cout<<"el resultado de la multiplicacion es:";c1*c2;}
-            break;
-    case 4:{cout<<"el resultado de la division es:";c1/c2;}
-            break;
-    case 5:{cout<<"el resultado del  conjugado #1 es:";~c1;}
-            break;
-    case 6:{cout<<"el resultado del  conjugado #2 es:";~c2;}
-            break;
-    case 7:{cout<<"el resultado del  modulo #1 es:";double e=Modulo(c1);cout<<e<<endl;}
-            break;
-    case 8:{cout<<"el resultado del  modulo #2 es:";double f=Modulo(c2);cout<<f<<endl;}
-            break;
 
-
-
-
-    }
-
-
-   return 0;
-
-}
-double Modulo(Complejo c1)
-{
-    double a=pow(c1.getreal_part(),2)+pow(c1.getimg_part(),2);
-    double b=sqrt(a);
-    return b;
-
+    case 3: {cout<<"3"<<endl;}
+        break;
+    case 4: {cout<<"4"<<endl;}
+        break;
+    case 5: {cout<<"5"<<endl;}
+        break;
 }
 
 
-
-
-
+    return 0;
+}
